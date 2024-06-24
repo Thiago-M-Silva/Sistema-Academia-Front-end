adbd8b2ebe9e7ced06cdf39c7ca80987ae0b2399
Uzeda <gabrielsuzeda@gmail.com>
2024-06-24 20:30:16 -0300

    login atualizado

--- pages/index.vue
+++ pages/index.vue
@@ -1,21 +1,82 @@
 <template>
-    <div class="flex items-center justify-center h-screen w-screen">
-      <div class="bg-gray-300 p-6 rounded-lg shadow-md m-8">
+     <div class="flex items-center primary-bg justify-center h-screen w-screen">
+    <div class="bg-white flex rounded-lg shadow-md m-8 w-1/2 h-3/5 divide-x divide-gray-300">
+      <!-- Parte da esquerda -->
+      <div class="w-2/5 p-6">
         <h2 class="text-xl font-semibold mb-4">Login</h2>
-        <form @submit.prevent="signIn">
+        <form>
           <div class="mb-4">
             <label for="username" class="block text-sm font-medium text-gray-700">Nome de usuário</label>
-            <input v-model="username" type="text" name="username" class="mt-1 p-2 border rounded-md w-full">
+            <input type="text" id="username" name="username" class="mt-1 p-2 border rounded-md w-full primary-text">
           </div>
           <div class="mb-4">
             <label for="password" class="block text-sm font-medium text-gray-700">Senha</label>
-            <input v-model="password" type="password" name="password" class="mt-1 p-2 border rounded-md w-full">
+            <input type="password" id="password" name="password" class="mt-1 p-2 border rounded-md w-full primary-text">
           </div>
-          <button type="submit" class="bg-blue-500 text-white px-4 py-2 rounded-md hover:bg-blue-600">Entrar</button>
+          <button type="submit" class="primary-bg text-white px-4 py-2 rounded-md hover:bg-blue-600">Entrar</button>
         </form>
       </div>
+      <!-- Parte da direita -->
+      <div class="w-3/5 teste">
+        <div id="login-illustration" style="border-radius: 0px 8px 8px 0px;" class="w-full h-full">
+
+        </div>
+      </div>
     </div>
+  </div>
   </template>
+
+
+<style>
+  .primary-bg {
+           background-color: #222366;
+       }
+
+
+       .primary-text {
+           color: #222366;
+       }
+
+
+       .secondary-bg {
+           background-color: #6b6c8c; /* Cor secundária */
+       }
+
+
+       .secondary-text {
+           color: #6b6c8c; /* Cor do texto secundário */
+       }
+
+
+       .tint-1-bg {
+           background-color: #484973; /* Tons mais claros para background */
+       }
+
+
+       .tint-1-text {
+           color: #484973; /* Tons mais claros para texto */
+       }
+
+
+       .tint-2-bg {
+           background-color: #151632; /* Tons mais escuros para background */
+       }
+
+
+       .tint-2-text {
+           color: #151632; /* Tons mais escuros para texto */
+       }
+
+
+       #login-illustration {
+           background-image: url("https://r4.wallpaperflare.com/wallpaper/199/924/33/muscle-muscle-bodybuilding-press-wallpaper-5e83ab4709bbfda7c62378900524157e.jpg");
+           background-size: cover;
+           background-position: center;
+           height: 100%;
+           width: 100%;
+           border-radius: 0px 8px 8px 0px;
+       }
+</style>
   
   <script lang="ts">
   import { ref } from 'vue';
