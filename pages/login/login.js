54aaf608b657ebeaab72515cc9d44d812734643f
Thiago <thiagobossun06@gmail.com>
2024-07-02 10:07:24 -0300

    Teste de redirecionamento

--- pages/login/login.js
+++ pages/login/login.js
@@ -1,17 +1,16 @@
 $(document).ready(function() {
     var username = window.document.getElementsById('username')
     var password = window.document.getElementsById('password')
-    var url
+    var url = 'http://localhost:8006/login'
 
     // ainda não testado
     document.addEventListener('DOMContentLoaded', function() {
         const apiUrl = {url}; // Substitua pela URL da sua API
-        const requestData = { username: {username}, password: {password} }; // Dados a serem enviados no corpo da requisição
+        const requestData = { username: {username}, password: {password} }; 
     
         httpPost(apiUrl, requestData).subscribe(data => {
           if (data) {
             console.log('Dados recebidos:', data);
-            // Manipule os dados recebidos aqui
           } else {
             console.log('Falha ao receber dados');
           }
