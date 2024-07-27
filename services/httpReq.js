06077f7e5fe4c2d175869d1d774172ddbe0ca97d
AndreFilho0 <filhoandre534@gmail.com>
2024-07-27 16:11:52 -0300

    passando a requisição para https

--- services/httpReq.js
+++ services/httpReq.js
@@ -2,7 +2,7 @@ import { CheckAuthenticationAndRedirect } from "../services/authUtils.js";
 
 export class HttpReq {
   constructor() {
-    this.url = "http://melivra.com:8008/api/";
+    this.url = "https://melivra.com:8008/api/";
     this.checkAuth = new CheckAuthenticationAndRedirect();
   }
 
